dfcecc2a802c83bcd89eeb2bd80be7488bc05952
aleckalina <72584564+aleckalina@users.noreply.github.com>
2021-01-15 15:22:25 +0900

    Add files via upload

---
+++ 3.3.py
@@ -0,0 +1,6 @@
+print('Введите 3 числа')
+a = int(input())
+b = int(input())
+c = int(input())
+z = (a+b+c)/3
+print('Среднее арифметическое этих чисел - ',z)
---
+++ 3.py
@@ -0,0 +1,4 @@
+def print_hello():
+ print('Привет')
+ print('Hello')
+ print('Hi')
